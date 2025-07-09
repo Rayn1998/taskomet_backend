fc444c634670955551320ded35a2aae35b5978b7
Yuriy Bodolanov <yuriy.bodolanov@gmail.com>
2025-07-09 18:10:54 +0300

    изменил .sql файл

--- table.sql
+++ table.sql
@@ -1,18 +1,16 @@
 CREATE TABLE artist (
  id SERIAL PRIMARY KEY NOT NULL,
  name VARCHAR(50) NOT NULL,
- role VARCHAR(50) NOT NULL,
+ role INT NOT NULL,
  tgId VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE tasks(
  id SERIAL PRIMARY KEY NOT NULL,
- deadline DATE NOT NULL,
  priority INTEGER NOT NULL,
  executor INTEGER NOT NULL,
- supervisor INTEGER NOT NULL,
  description VARCHAR(500) NOT NULL,
- project VARCHAR(50) NOT NULL,
+ project INT NOT NULL,
  scene VARCHAR(50) NOT NULL,
  status INTEGER NOT NULL,
  shot VARCHAR(50) NOT NULL
@@ -22,17 +20,4 @@ CREATE TABLE projects(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(1000)
-);
-
-INSERT INTO tasks (project, scene, shot, description, status, priority, executor, supervisor, deadline)
-VALUES (
- 'HOL',
- 'AH-014',
- '0110',
- 'трек сцены и портала, трек правой по кадру кисти',
- 1,
- 1,
- 1,
- 2,
- '2025-07-10'
 );
